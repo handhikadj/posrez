db1d7d25200c923cbf3b26092a01b2a5f374c605
Handika Dwi Julianto <dadangsukatoro@gmail.com>
2018-08-23 17:49:52 +0700

    Add SQL

--- posyandu.sql
+++ posyandu.sql
@@ -1,20 +1,22 @@
 -- phpMyAdmin SQL Dump
--- version 3.5.2.2
--- http://www.phpmyadmin.net
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 30, 2016 at 06:20 AM
--- Server version: 5.5.27
--- PHP Version: 5.4.7
+-- Generation Time: Aug 23, 2018 at 12:48 PM
+-- Server version: 10.1.30-MariaDB
+-- PHP Version: 7.2.1
 
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `posyandu`
@@ -26,12 +28,11 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `admin` (
+  `id_admin` int(11) NOT NULL,
   `nama_admin` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_admin`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `admin`
@@ -46,7 +47,7 @@ INSERT INTO `admin` (`id_admin`, `nama_admin`, `password`) VALUES
 -- Table structure for table `anak`
 --
 
-CREATE TABLE IF NOT EXISTS `anak` (
+CREATE TABLE `anak` (
   `id_anak` varchar(20) NOT NULL,
   `nama_anak` varchar(100) NOT NULL,
   `tanggal_lahir` date NOT NULL,
@@ -56,8 +57,7 @@ CREATE TABLE IF NOT EXISTS `anak` (
   `alamat` varchar(255) NOT NULL,
   `panjang_badan` int(11) NOT NULL,
   `berat_lahir` int(11) NOT NULL,
-  `lingkar_kepala` int(11) NOT NULL,
-  PRIMARY KEY (`id_anak`)
+  `lingkar_kepala` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -65,7 +65,9 @@ CREATE TABLE IF NOT EXISTS `anak` (
 --
 
 INSERT INTO `anak` (`id_anak`, `nama_anak`, `tanggal_lahir`, `jenis_kelamin`, `nama_ibu`, `nama_ayah`, `alamat`, `panjang_badan`, `berat_lahir`, `lingkar_kepala`) VALUES
-('B0001', 'Syaiful Nazar', '2016-11-30', 'L', 'Khoimaroh', 'Kartaji', 'Sambogunung', 1, 1, 1);
+('B0001', 'Syaiful Nazar', '2016-11-30', 'L', 'Khoimaroh', 'Kartaji', 'Sambogunung', 1, 1, 1),
+('B0002', 'Reza', '2018-08-28', 'P', 'Bambang', 'Bambang', 'Bambang', 30, 30, 15),
+('B0003', 'Fathur', '2018-06-20', 'Perempuan', 'asdasdas', 'asdasdasd', 'asdasd', 0, 0, 0);
 
 -- --------------------------------------------------------
 
@@ -73,11 +75,10 @@ INSERT INTO `anak` (`id_anak`, `nama_anak`, `tanggal_lahir`, `jenis_kelamin`, `n
 -- Table structure for table `imunisasi`
 --
 
-CREATE TABLE IF NOT EXISTS `imunisasi` (
+CREATE TABLE `imunisasi` (
   `id_imunisasi` int(11) NOT NULL,
   `jenis_imunisasi` varchar(20) NOT NULL,
-  `usia_wajib` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_imunisasi`)
+  `usia_wajib` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -94,22 +95,20 @@ INSERT INTO `imunisasi` (`id_imunisasi`, `jenis_imunisasi`, `usia_wajib`) VALUES
 -- Table structure for table `kematian`
 --
 
-CREATE TABLE IF NOT EXISTS `kematian` (
-  `id_kematian` int(11) NOT NULL,
-  `id_anak` int(11) NOT NULL,
+CREATE TABLE `kematian` (
+  `id_kematian` varchar(20) NOT NULL,
+  `id_anak` varchar(20) NOT NULL,
   `tanggal_kematian` date NOT NULL,
   `keterangan` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_kematian`)
+  `stat` int(11) DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `kematian`
 --
 
-INSERT INTO `kematian` (`id_kematian`, `id_anak`, `tanggal_kematian`, `keterangan`) VALUES
-(1, 1, '2016-10-09', 'Kecelakaan'),
-(2, 1, '2016-10-25', 'Sakit'),
-(3, 1, '2016-10-19', 'Kecelakaan');
+INSERT INTO `kematian` (`id_kematian`, `id_anak`, `tanggal_kematian`, `keterangan`, `stat`) VALUES
+('K0001', 'B0001', '2018-08-21', 'asdasd', 1);
 
 -- --------------------------------------------------------
 
@@ -117,18 +116,33 @@ INSERT INTO `kematian` (`id_kematian`, `id_anak`, `tanggal_kematian`, `keteranga
 -- Table structure for table `penimbangan`
 --
 
-CREATE TABLE IF NOT EXISTS `penimbangan` (
+CREATE TABLE `penimbangan` (
   `id_penimbangan` varchar(20) NOT NULL,
-  `id_anak` int(11) NOT NULL,
+  `id_anak` varchar(20) NOT NULL,
   `tanggal_timbang` date NOT NULL,
   `usia` varchar(20) NOT NULL,
   `berat_badan` int(11) NOT NULL,
   `lingkar_perut` int(11) NOT NULL,
   `id_imunisasi` int(11) NOT NULL,
   `id_vitamin` int(11) NOT NULL,
-  `saran` varchar(50) NOT NULL,
-  PRIMARY KEY (`id_penimbangan`)
+  `saran` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `penimbangan`
+--
+
+INSERT INTO `penimbangan` (`id_penimbangan`, `id_anak`, `tanggal_timbang`, `usia`, `berat_badan`, `lingkar_perut`, `id_imunisasi`, `id_vitamin`, `saran`) VALUES
+('P0001', 'B0002', '2018-08-14', '17', 12312312, 17, 1, 2, 'anjing'),
+('P0003', 'B0001', '2018-08-08', '123123', 123123, 123123, 1, 1, '123123'),
+('P0004', 'B0002', '2018-08-01', '123123', 123123, 123123, 1, 1, '123123'),
+('P0005', 'B0002', '2018-08-22', '50', 5, 50, 2, 2, '12312123'),
+('P0006', 'B0001', '2018-08-14', '12123123', 1231231, 1231231, 2, 2, '123123123'),
+('P0007', 'B0001', '2018-07-03', '50', 50, 50, 1, 1, 'asdsdasd'),
+('P0008', 'B0002', '2018-08-08', '60', 60, 60, 2, 2, 'asdasdasdasd'),
+('P0009', 'B0002', '2018-07-11', '123123', 123123, 12123, 2, 2, '12312123'),
+('P0010', 'B0001', '2018-06-19', '123123', 123123, 123123, 1, 1, 'qweqwqwe'),
+('P0011', 'B0001', '2018-08-22', 'asdasad', 0, 0, 2, 2, 'asdasdasd');
 
 -- --------------------------------------------------------
 
@@ -136,11 +150,10 @@ CREATE TABLE IF NOT EXISTS `penimbangan` (
 -- Table structure for table `vitamin`
 --
 
-CREATE TABLE IF NOT EXISTS `vitamin` (
+CREATE TABLE `vitamin` (
   `id_vitamin` int(11) NOT NULL,
   `jenis_vitamin` varchar(20) NOT NULL,
-  `usia_wajib` varchar(10) NOT NULL,
-  PRIMARY KEY (`id_vitamin`)
+  `usia_wajib` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -150,6 +163,75 @@ CREATE TABLE IF NOT EXISTS `vitamin` (
 INSERT INTO `vitamin` (`id_vitamin`, `jenis_vitamin`, `usia_wajib`) VALUES
 (1, 'Vitamin A Biru', ''),
 (2, 'Vitamin A Merah', '');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id_admin`);
+
+--
+-- Indexes for table `anak`
+--
+ALTER TABLE `anak`
+  ADD PRIMARY KEY (`id_anak`);
+
+--
+-- Indexes for table `imunisasi`
+--
+ALTER TABLE `imunisasi`
+  ADD PRIMARY KEY (`id_imunisasi`);
+
+--
+-- Indexes for table `kematian`
+--
+ALTER TABLE `kematian`
+  ADD PRIMARY KEY (`id_kematian`),
+  ADD KEY `id_anak` (`id_anak`);
+
+--
+-- Indexes for table `penimbangan`
+--
+ALTER TABLE `penimbangan`
+  ADD PRIMARY KEY (`id_penimbangan`),
+  ADD KEY `id_anak` (`id_anak`);
+
+--
+-- Indexes for table `vitamin`
+--
+ALTER TABLE `vitamin`
+  ADD PRIMARY KEY (`id_vitamin`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `admin`
+--
+ALTER TABLE `admin`
+  MODIFY `id_admin` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `kematian`
+--
+ALTER TABLE `kematian`
+  ADD CONSTRAINT `kematian_ibfk_1` FOREIGN KEY (`id_anak`) REFERENCES `anak` (`id_anak`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `penimbangan`
+--
+ALTER TABLE `penimbangan`
+  ADD CONSTRAINT `penimbangan_ibfk_1` FOREIGN KEY (`id_anak`) REFERENCES `anak` (`id_anak`) ON DELETE CASCADE;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
